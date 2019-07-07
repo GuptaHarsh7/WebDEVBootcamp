a8d53931ecf30795ad779e2d2af8448fd95bf1ed
Harsh Gupta <harshgupta6641@gmail.com>
2019-07-07 19:14:52 +0000

    m

--- moreexpress/EJSDemo/app.js
+++ moreexpress/EJSDemo/app.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req,res){
-  res.send("Welcome to home page!");
+    res.render("home.ejs");
 });
+
+app.get("/fallinlovewith/:thing",function(req,res){
+    var thing = req.params.thing;
+    res.render("love.ejs");
+})
 
 app.listen("3000",process.env.IP,function(){
     console.log("Server is listening");
