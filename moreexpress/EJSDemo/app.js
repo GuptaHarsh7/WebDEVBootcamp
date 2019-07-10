5fd32cf46966ce9c3bc4589b3a23df070b5e344a
Harsh Gupta <harshgupta6641@gmail.com>
2019-07-10 14:53:59 +0000

    m

--- moreexpress/EJSDemo/app.js
+++ moreexpress/EJSDemo/app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing",function(req,res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thingVar : thing});
+    res.render("love",{thingVar : thing});
 });
 
 app.get("/posts",function(req,res){
@@ -16,12 +19,13 @@ app.get("/posts",function(req,res){
     {title:"My Adorable pet",author:"Shin"},
     {title:"Can you believe this!! ",author:"Taro"}
   ];
-  res.render("posts.ejs",{posts:posts});
+  res.render("posts",{posts:posts});
 });
+
 app.get("*",function(req,res){
   res.send("NO SUCH PAGE");
 });
 
 app.listen("3000",process.env.IP,function(){
     console.log("Server is listening");
-})
+});
